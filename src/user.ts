78adf98c4384ea86b7d5f1cf26679ddaa75d8632
samekmd <samuelalkmc@gmail.com>
2024-09-09 10:43:18 -0300

    Criando classe usuário e configurando tsconfig

---
+++ src/user.ts
@@ -0,0 +1,20 @@
+export default abstract class User{
+
+    private name: string;
+    private email: string;
+    private password: string;
+    
+    constructor(name: string, email: string, password: string){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+
+
+    abstract login(): void;
+    abstract logout(): void;
+    
+
+    
+}
