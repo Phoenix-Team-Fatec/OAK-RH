e04df8ee353f7716ae57ebe109476810f8bebe9e
Guilherme Sato <guilherme.yokota20@gmail.com>
2024-09-09 19:48:24 -0300

    Criando classe Admin

---
+++ src/admin.ts
@@ -0,0 +1,24 @@
+import User from "./user";
+
+export default class Admin extends User{
+    constructor(name: string, email: string, password: string){
+        super(name, email, password)
+    }
+
+    login(): void{
+
+    }
+
+    logout(): void{
+
+    }
+
+    cadastrarUsuario(): void{
+
+    }
+
+    classificarUsuario(): void{
+
+    }
+
+}
