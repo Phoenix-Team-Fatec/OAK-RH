a860df774f741f64a5999e4536005dd46b2d662c
Peretta <viniassisperetta@gmail.com>
2024-09-10 07:42:30 -0300

    classe lider criada

---
+++ src/classes/lider.ts
@@ -0,0 +1,17 @@
+import User from "./user";
+
+export default class Lider extends User
+{
+    
+    constructor(name: string, email: string, password: string){
+        super(name, email, password)
+    }
+
+    login(): void {
+        throw new Error("Method not implemented.");
+    }
+    logout(): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
